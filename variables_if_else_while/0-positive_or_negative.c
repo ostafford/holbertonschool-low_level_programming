069b2fecf08e14175040049f467f36f35cb7e02f
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-24 10:58:20 +1100

    created file 0....

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("is positive");
+	}
+	else if (n == 0)
+	{
+	printf("is zero");
+	}
+	else
+	(n < 0)
+	{
+	printf("is negative\n");
+	}
+return (0);
+}
