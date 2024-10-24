f183150fc922a007d77a857990193cdeca809d32
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-24 22:00:37 +1100

    created file 5...

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -19,3 +19,11 @@ putchar(chlow);
 putchar('\n');
 return (0);
 }
+
+/*
+- The char data type variable chlow is initialized to the value 'a', representing the starting point of the alphabet.
+- The for loop iterates through each letter from 'a' to 'z', incrementing by 1 with chlow++ (going through the alphabet).
+- The if statement uses the != operator to exclude the letters 'e' and 'q' from being processed in the loop.
+- For each character that is not 'e' or 'q', putchar(chlow) prints that character to the console.
+- Finally, putchar('\n') prints a newline to move the cursor to the next line after all the characters have been printed.
+ */
