6e050a3dc84eb1b57b2a219ac33a05404d445bf9
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-24 21:53:46 +1100

    created file 4...

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -19,11 +19,3 @@ putchar(chlow);
 putchar('\n');
 return (0);
 }
-
-/* 
-1. Data Type: The char data type represents a single character. In this case, the variable ch is initialized with the value 'a'.
-2. Looping: The for loop starts with ch set to 'a', and in each iteration, ch is incremented by 1 (using ch++). The loop continues until ch exceeds 'z'.
-3. Increment: The ch++ part increases the character's ASCII value, so it moves from 'a' to 'b', then 'b' to 'c', and so on, until 'z'.
-4. Printing: The putchar(ch) function prints the current character in each iteration of the loop. It prints each character one by one as the loop iterates.
-5. New Line: After all characters from 'a' to 'z' are printed, the putchar('\n') adds a new line for better formatting.
- */
