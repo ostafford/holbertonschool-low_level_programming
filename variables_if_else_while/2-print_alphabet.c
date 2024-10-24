83110ffc5ac299d26b6a877740c27314985d18b5
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-24 12:51:40 +1100

    created file 3...

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (char c = 65; c <= 90; c++)
+{
+putchar(c);
+}
+return (0);
+}
