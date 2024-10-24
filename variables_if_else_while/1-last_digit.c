248883aa595bbf2fc42a829f0f229a2f530968de
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-24 12:28:19 +1100

    created file 1.....

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int l;
 l = n % 10;
 if (l > 5)
 {
-printf("Last digit of %d is %d and greater than 5\n", n, l);
+printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l == 0)
 {
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else if (l < 6)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0/n", n, l);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 	return (0);
 }
