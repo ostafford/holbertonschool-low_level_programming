1152073b8719b6bbb89c905844a846249ea7c164
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-25 22:30:42 +1100

    Created file 2... performed betty check

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times
- *
- * Return: 0
- */
+* main - check the code.
+*
+* Return: Always 0.
+*/
 void print_alphabet_x10(void)
 {
 	char c, i;
