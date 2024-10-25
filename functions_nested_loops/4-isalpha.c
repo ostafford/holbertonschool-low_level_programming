251c5327f04ec97ad5ad1c66f6ad0bd82c562bf4
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-25 23:46:19 +1100

    created file 4...passed betty check.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _islower - function to check for lowercase character
+ * _isalpha - function that checks for alphabetic character.
  * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
 		return (1);
 	}
