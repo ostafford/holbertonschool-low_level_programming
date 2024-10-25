10f8fa9c4d58544496c1a742f7b5d43c988ef6a7
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-26 00:00:56 +1100

    created file 5...passed betty check.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,13 +9,13 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	printf('+');
+	_putchar('+');
 	return (1);
 } else if (n < 0)
 {
-	printf('-');
+	_putchar('-');
 	return (0);
 } else
-	printf('0');
+	_putchar('0');
 	return (0);
 }
