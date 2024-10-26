cabfc3f162ed7ed204052e6c1b32642ad2ce0bb2
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-26 23:09:05 +1100

    created file 7...resolved checker error

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_last_digit - function that prints the last digit of a number.
- * @l:  is the int that will use for the argument of the function
+ * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-
-int print_last_digit(int l)
+int print_last_digit(int c)
 {
-if (l > 0 || l == 0)
-{
-_putchar (l % 10 + '0');
-return (l % 10);
-}
-else
-{
-l = l * -1;
-_putchar (l % 10 + '0');
-return (l % 10);
-}
+	if (c > 0 || c == 0)
+	{
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
+	else
+	{
+	c = c * -1;
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
 }
