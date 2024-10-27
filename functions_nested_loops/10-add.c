d9e4be5f12c231569392310d66e19eb8a5031f06
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-27 23:38:50 +1100

    updated file 10...

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - add two integers and returns the result
  * @a: int to be added to b
  * @b: int to be added to a
- * return: sum of a and b
+ * Return: sum of a and b
  */
 
 int add(int a, int b)
