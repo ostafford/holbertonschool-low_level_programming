4ab85d74f5a66edf0c60cde8f4dcd3d3afacac64
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-27 23:37:07 +1100

    updated file 9...

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * time_table - function that prints the 9 times table, starting with 0
+ * times_table - function that prints the 9 times table, starting with 0
  *
  */
 
-void time_table(void)
+void times_table(void)
 {
 	int i, j, k;
 
