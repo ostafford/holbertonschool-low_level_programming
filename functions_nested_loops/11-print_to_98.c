cc210dcfe5c46793daa6100400474ba5b0afcc3a
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-27 23:01:07 +1100

    created file 11...

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				_putchar("%d, ", i);
+				printf("%d, ", i);
 			else if (i == 98)
-				_putchar("%d\n", i);
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (s = n; s >= 98; s--)
 		{
 			if (s != 98)
-				_putchar("%d, ", s);
+				printf("%d, ", s);
 			else if (j == 98)
-				_putchar("%d\n", s);
+				printf("%d\n", s);
 		}
 	}
 }
