d20490d8ba0a5bc64bc8af02b7cd6a4cfd0cd558
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-27 22:37:29 +1100

    created file 7...resolved checker error

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,12 +7,15 @@
 
 int print_last_digit(int l)
 {
-int last_digit;
-
-last_digit = l % 10;
-if (last_digit < 0)
-last_digit *= -1;
-
-_putchar (last_digit + '0');
-return (last_digit);
+if (l > 0 || l == 0)
+{
+_putchar (l % 10 + '0');
+return (l % 10);
+}
+else
+{
+l = l * -1;
+_putchar (l % 10 + '0');
+return (l % 10);
+}
 }
