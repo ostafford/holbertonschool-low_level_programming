6873f09316b97aacb921fbbf4b73a1933f2994a8
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-27 22:56:26 +1100

    created file 11...

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * insert new line
+ * @num: print from this number 
+ * return: sum of a and b
+ */
+
+void print_to_98(int num)
+{
+	int i, s;
+
+	if (num <= 98)
+	{
+		for (i = num; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (num >= 98)
+	{
+		for (s = num; s >= 98; s--)
+		{
+			if (s != 98)
+				printf("%d, ", s);
+			else if (j == 98)
+				printf("%d\n", s);
+		}
+	}
+}
