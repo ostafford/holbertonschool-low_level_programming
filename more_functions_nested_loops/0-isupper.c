c0c9bb2bf1cbced91d843bf28de0a7e09747e8ef
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-28 22:21:54 +1100

    edited and updated file 0...

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 
 int _isupper(int c){
 
-return (c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <= 'Z') {
+    return 1; 
+} else {
+    return 0; 
 }
-
-
+}
