1049863d4fb1cd3dcd82c1e3ca0fcdd6479ec027
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-30 14:08:10 +1100

    updated file 4...

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9.
+ *
+ * Return: value of 0.
+ */
+
+void print_most_numbers(void)
+{
+char i;
+for (i = 0; i < 10; i++)
+{
+if (i != '2' && i != '4')
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
