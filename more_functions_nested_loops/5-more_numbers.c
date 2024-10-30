517792b02d2ade1f591abda858f18260fd3e443d
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-30 14:23:41 +1100

    updated file 5...

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,15 @@ void more_numbers(void)
 {
 char i;
 char n;
-for (i = 0; i < 14; i++)
+for (i = 0; i < 10; i++)
 {
-for (n = 0; n < 10; n++)
+for (n = 0; n < 15; n++)
 {
-_putchar(n + '0');
+if (n>=10)
+{
+_putchar(n/10 +'0');
+}
+_putchar(n % 10 + '0');
 }
 _putchar('\n');
 }
