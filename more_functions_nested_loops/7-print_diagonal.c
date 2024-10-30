9095fbceb9403ed6e9293a8f377ecf20c5f624b1
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-30 14:46:58 +1100

    updated file 7...

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,23 @@
 
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-int l;
-for (l = 1; l <= n; l++)
-{
-_putchar('\\');
-}
-_putchar('\n');
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int l, slash;
+
+		for (l = 0; l < n; l++)
+		{
+			for (slash = 0; slash < n; slash++)
+			{
+				if (slash == slash)
+					_putchar('\\');
+				else if (slash < l)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
