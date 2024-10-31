141bb7a7fdbed2d2b432e00634e3264c3dc8743d
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-31 15:25:14 +1100

    created file 1...

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*swap_int - swaps the values of two integers.
+*@a: needs to equal b
+*@b: needs to equal a
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
