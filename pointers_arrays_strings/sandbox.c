f58ef3537795414adedbd10eaccb267952bab5b4
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-31 15:32:30 +1100

    updated file 1....

--- pointers_arrays_strings/sandbox.c
+++ pointers_arrays_strings/sandbox.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
+    int a;
+    int b;
 
-    return 0;
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    // swap_int(&a, &b)
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }
+
