9b58c8208c2c50ebc356ade2e1afdaca09cc60d9
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 09:56:17 +1100

    created file 2...

--- pointers_arrays_strings/sandbox.c
+++ pointers_arrays_strings/sandbox.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-    char i;
-    char l;
+	int i;
 
-    i = 0;
-    l = 0;
+	i = 0;
 
-    while (dest[i] != '\0')
-        i = i + 1;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-        while (src[l] != '\0' && l < n)
-        {
-            dest[i] = src[l];
-        l = l + 1;
-        i = i + 1;
-        }
-
-    {
-        dest[i] = '\0';
-    }
-    return(dest);
-
+	return (dest);
 }
 
 
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
+    char s1[98];
     char *ptr;
+    int i;
 
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
     printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
     printf("%s\n", s1);
-    printf("%s", s2);
     printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
     printf("%s", s1);
-    printf("%s", s2);
     printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
     return (0);
 }
