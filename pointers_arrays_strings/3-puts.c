362343f3f81474d77cbaacb9cbbd46de1c9fd139
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-31 17:30:29 +1100

    created file 3...

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: pointer to print string
+ *
+ * Return:
+ */
+
+void _puts(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+_putchar(stdout[i]);
+}
+_putchar('\n');
+return (0);
+}
