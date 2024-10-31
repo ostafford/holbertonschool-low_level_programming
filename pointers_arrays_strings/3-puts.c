6aace2cce97a4964b17100224a2e6becbf685f78
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-10-31 17:35:55 +1100

    created file 3...

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -10,12 +10,11 @@
 
 void _puts(char *str)
 {
-int i;
-i = 0;
-while (str[i] != '\0')
-{
-printf(str[i]);
-i = i +1;
-}
-printf('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
