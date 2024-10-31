e562ece4712ef98065e0d18c38cc443b3803227f
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 09:24:15 +1100

    created file 0...

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -18,6 +18,7 @@ i = 0;
 n = 0;
 
 while (dest[i] != '\0')
+{
 i = i + 1;
 
 while (src[n] != '\0')
@@ -26,8 +27,6 @@ dest[i] = src[n];
 n = n + 1;
 i = i + 1;
 }
-
-{
 dest[i] = '\0';
 }
 return (dest);
