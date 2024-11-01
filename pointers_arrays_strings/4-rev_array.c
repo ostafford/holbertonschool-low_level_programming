5112130073b11b33b3803d08fbbd186eaa689524
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 11:44:14 +1100

    created file 4...

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,13 +11,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, 
-    j, 
-    tmp;
+	int i,
+	j,
+	tmp;
 
 	j = n - 1;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n / 2; i = i + 1)
 	{
 		tmp = a[i];
 		a[i] = a[j];
