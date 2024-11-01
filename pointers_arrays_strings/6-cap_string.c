6b71ece894dce9f89e1673b4d11f1da432591a15
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 12:07:56 +1100

    created file 5...

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes every word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	char special_char[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+	}
+
+	return (s);
+}
