c4f2c9e07cd4d335d56cdba6fabd96597a2b8055
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 11:56:31 +1100

    created file 5...

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,6 +12,12 @@
 char *string_toupper(char *s)
 {
 	int i;
-for (i = 0; s[i] != '\0'; i = i + 1)
-return (s);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
 }
