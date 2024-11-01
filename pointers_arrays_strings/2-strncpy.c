aa35b9ad25cc03217796fc7abf41b51797b0bf8d
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 11:13:01 +1100

    created file 2...

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,11 +22,10 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i = i + 1;
 	}
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i = i + 1;
-    }
-
+while (i < n)
+{
+dest[i] = '\0';
+i = i + 1;
+}
 	return (dest);
 }
