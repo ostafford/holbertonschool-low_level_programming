738753571323dac5c5e322c8952bc11b67b5a9a2
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 14:10:57 +1100

    created file 6...

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,26 @@
 char *cap_string(char *s)
 {
 	int i;
+	int ary_lp;
 
-	char special_char[13] = {' ', '\t', '\n', ',', ';', '.',
+	char spc_char[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i = i + 1)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
+		for (ary_lp = 0; ary_lp < 13; ary_lp = ary_lp + 1)
+		{
+			if (s[i] == spc_char[ary_lp])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
 
 	return (s);
