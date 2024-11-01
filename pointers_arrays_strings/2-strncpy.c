c07e94e3ed0aeccbe842de9cb72deb300ca2e600
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 11:11:36 +1100

    created file 2...

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -20,8 +20,13 @@ char *_strncpy(char *dest, char *src, int n)
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
-		i++;
+		i = i + 1;
 	}
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i = i + 1;
+    }
 
 	return (dest);
 }
--- pointers_arrays_strings/sandbox.c
+++ pointers_arrays_strings/sandbox.c
@@ -11,6 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
+    if (i < n)
+    {
+        dest[i] = '\0';    
+        i = i + 1;
+    }
+    
 
 	return (dest);
 }
