1ef218cd047f714a5a292b56b639a4ff75e49dcc
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 11:53:50 +1100

    created file 5...

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+for (i = 0; s[i] != '\0'; i = i + 1)
+return (s);
+}
