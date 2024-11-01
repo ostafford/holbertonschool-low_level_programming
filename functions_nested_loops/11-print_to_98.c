e4d8838452c6bb19d687e735ae5c3b831f1bbc43
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-01 14:41:02 +1100

    updated files from previous project

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,20 +12,20 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i = i + 1)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
-			else
+			else if (i == 98)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
-		for (j = n; j >= 98; j = j - 1)
+		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
 				printf("%d, ", j);
-			else
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}
