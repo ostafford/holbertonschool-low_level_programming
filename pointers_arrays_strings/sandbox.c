d51bcfe3d4bd1b63a72c2d05efa4097d44237e35
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-06 10:38:05 +1100

    created file 0....

---
+++ pointers_arrays_strings/sandbox.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/* Function Prototype */
+char *_memset(char *s, char buffer, unsigned int n);
+
+/* Data Segment */
+
+/* BSS Segment */
+
+/* Stack */
+char *_memset(char *s, char buffer, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i = i + 1)
+    {
+        s[i] = buffer;
+    }
+}
+
+/* Text Segment */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
