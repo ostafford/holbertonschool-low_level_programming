e5c674376e2dc53347bf94f9202c084b433d4ea7
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-06 21:46:40 +1100

    created file 8....

--- pointers_arrays_strings/sandbox.c
+++ pointers_arrays_strings/sandbox.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /* Function Prototype */
-void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 /* Data Segment */
 
@@ -11,34 +11,40 @@ void print_chessboard(char (*a)[8]);
 
 
 /* Stack */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int i; 
-	int index_match;
+	int i;
 
-	for (i = 0; i < 8; i = i + 1)
+	unsigned int diag_1, diag_2;
+
+	diag_1 = 0;
+	diag_2 = 0;
+
+	for (i = 0; i < size; i = i + 1)
 	{
-		for (index_match = 0; index_match < 8; index_match = index_match + 1)
-		{
-			putchar(a[i][index_match]);
-		}
-		putchar('\n');
+		diag_1 += a[(size * i) + i];
+		diag_2 += a[(size * (i + 1)) - (i + 1)];
 	}
+
+	printf("%d, %d\n", diag_1, diag_2);
 }
 
 /* Text Segment */
 int main(void)
 {
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    int c3[3][3] = {
+        {0, 1, 5},
+        {10, 11, 12},
+        {1000, 101, 102},
     };
-    print_chessboard(board);
+    int c5[5][5] = {
+        {0, 1, 5, 12124, 1234},
+        {10, 11, 12, 123521, 12512},
+        {1000, 101, 102, 12545, 214543435},
+        {100, 1012451, 11102, 12545, 214543435},
+        {10, 12401, 10452, 11542545, 1214543435},
+    };
+    print_diagsums((int *)c3, 3);
+    print_diagsums((int *)c5, 5);
     return (0);
 }
