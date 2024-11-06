e6bbcfc33af9e2bd4ff858b6d7941a2f5f2a9b84
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-06 13:34:23 +1100

    created file 2....

--- pointers_arrays_strings/sandbox.c
+++ pointers_arrays_strings/sandbox.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /* Function Prototype */
-char *_memcpy(char *buffer, char *buffer2, unsigned int n);
-
+char *_strchr(char *s, char c);
 /* Data Segment */
 
 
@@ -11,27 +10,35 @@ char *_memcpy(char *buffer, char *buffer2, unsigned int n);
 
 
 /* Stack */
-char *_memcpy(char *buffer, char *buffer2, unsigned int n)
+char *_strchr(char *s, char c)
 {
-    unsigned int i;
-    
-    for (i = 0; i < n; i = i + 1)
-    {
-        buffer[i] = buffer2[i];
-    }
-    
-    return (buffer);
+		int a;
+
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
 
 /* Text Segment */
 int main(void)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    char *s = "hello";
+    char *f;
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
