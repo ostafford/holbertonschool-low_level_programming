de96b8ebc4763db8887b1783599db840c0cadf53
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-07 11:52:13 +1100

    created file 1....

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,7 +11,8 @@
 int main(int argc, char *argv[])
 {
 
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
+
 (void)argv;
 
 return (0);
--- argc_argv/sandbox.c
+++ argc_argv/sandbox.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[]);
 int main(int argc, char *argv[])
 {
 
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
+
 (void)argv;
 
 return (0);
