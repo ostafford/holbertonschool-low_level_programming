705fcaad5e607abc157bb37ce87ddad677457489
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-07 14:16:58 +1100

    created file 1....

---
+++ recursion/sandbox.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/* Function Prototype */
+void _puts_recursion(char *s);
+
+/* Data Segment */
+
+
+/* BSS Segment */
+
+
+/* Stack */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+
+/* Text Segment */
+
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
