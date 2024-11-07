c28ae08136e227bf529b2e7c4cc6dd4999e3187a
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-07 18:06:24 +1100

    created file 6....

--- recursion/sandbox.c
+++ recursion/sandbox.c
@@ -2,28 +2,28 @@
 #include <stdio.h>
 
 /* Function Prototype */
-int _sqrt_recursion(int n);
-int inner_sqrt_recursion(int n, int i);
+int is_prime_number(int n);
+int final_prime(int n, int i);
 
 /* Data Segment */
 
 /* BSS Segment */
 
 /* Stack */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (inner_sqrt_recursion(n, 0));
+	if (n <= 1)
+		return (0);
+	return (final_prime(n, n - 1));
 }
 
-int inner_sqrt_recursion(int n, int i)
+int final_prime(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
-    if (i * i == n)
-        return (i);
-    return (inner_sqrt_recursion(n, i + 1));
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (final_prime(n, i - 1));
 }
 
 /* Text Segment */
@@ -31,17 +31,22 @@ int main(void)
 {
     int r;
 
-    r = _sqrt_recursion(1);
+    r = is_prime_number(1);
     printf("%d\n", r);
-    r = _sqrt_recursion(1024);
+    r = is_prime_number(1024);
     printf("%d\n", r);
-    r = _sqrt_recursion(16);
+    r = is_prime_number(16);
     printf("%d\n", r);
-    r = _sqrt_recursion(17);
+    r = is_prime_number(17);
     printf("%d\n", r);
-    r = _sqrt_recursion(25);
+    r = is_prime_number(25);
     printf("%d\n", r);
-    r = _sqrt_recursion(-1);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
     printf("%d\n", r);
     return (0);
 }
+
