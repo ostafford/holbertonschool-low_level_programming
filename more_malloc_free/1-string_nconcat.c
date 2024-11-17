2cb5b98c20c16e6b4292aaca9a35a69f078305b7
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-17 23:55:34 +1100

    created file 1...

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -34,10 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j = j + 1];
+		s[i++] = s2[j++];
 
 	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j = j + 1];
+		s[i++] = s2[j++];
 
 	s[i] = '\0';
 
