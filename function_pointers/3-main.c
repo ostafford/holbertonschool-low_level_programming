1cafe60479e1c7f6a9e6f770845eb2556d565888
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-20 13:26:03 +1100

    created file `multiple files...

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include "3-calc.h"
 
-
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 /* Function Prototype */
 
 
